ec1b3416e147a854e7f4c698e571bc967bad2d34
zxyle <zxyful@gmail.com>
2020-03-03 10:23:55 +0800

    Support Clang time format

--- timeutil/lib.go
+++ timeutil/lib.go
@@ -2,6 +2,7 @@ package timeutil
 
 import (
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -21,16 +22,9 @@ func GetTS() string {
 	return timeStamp(1e9)
 }
 
-// 获取当前日期 格式如: 20200104
-func getDate() string {
-	timeStr := time.Now().Format("20060102")
-	return timeStr
-}
-
 // 获取当前时间 格式如: 20200104234744
 func getTime() string {
-	timeStr := time.Now().Format("20060102150405")
-	return timeStr
+	return Format("%Y%m%d%X")
 }
 
 // 获取几天前的日期
@@ -40,6 +34,43 @@ func daysAfter(days int) string {
 
 // 获取今天日期的字符串
 func getTodayStr() string {
-	timeStr := time.Now().Format("20060102")
+	return Format("%Y%m%d")
+}
+
+func Format(format string) string {
+	format = strings.Replace(format, "%Y", "2006", -1)
+	format = strings.Replace(format, "%y", "06", -1)
+	format = strings.Replace(format, "%m", "01", -1)
+	format = strings.Replace(format, "%d", "02", -1)
+	// %w
+	format = strings.Replace(format, "%I", "03", -1)
+	format = strings.Replace(format, "%p", "PM", -1)
+	format = strings.Replace(format, "%f", ".000000", -1)
+
+	format = strings.Replace(format, "%x", "01/02/06", -1)
+	format = strings.Replace(format, "%X", "15:04:05", -1)
+	format = strings.Replace(format, "%H", "15", -1)
+	format = strings.Replace(format, "%M", "04", -1)
+	format = strings.Replace(format, "%S", "05", -1)
+
+	// 周几
+	format = strings.Replace(format, "%a", "Mon", -1)
+	format = strings.Replace(format, "%A", "Monday", -1)
+
+	// 月份
+	format = strings.Replace(format, "%b", "Jan", -1)
+	format = strings.Replace(format, "%B", "January", -1)
+
+	// %z
+	format = strings.Replace(format, "%Z", "MST", -1)
+	timeStr := time.Now().Format(format)
+
+	// %j
+	// %U
+	// %W
+	// %c
+	// %G
+	// %u
+	// %V
 	return timeStr
 }
